9864af1cbe5fe808704bbe12a448de924abd8108
Graeme Rocher <graeme.rocher@gmail.com>
2020-06-01 12:22:00 +0200

    Bump checkstyle version

--- src/main/groovy/io/micronaut/build/MicronautBuildExtension.groovy
+++ src/main/groovy/io/micronaut/build/MicronautBuildExtension.groovy
@@ -5,7 +5,7 @@ class MicronautBuildExtension {
     String sourceCompatibility = '1.8'
     String targetCompatibility = '1.8'
 
-    String checkstyleVersion = '8.32'
+    String checkstyleVersion = '8.33'
 
     String dependencyUpdatesPattern = /.+(-|\.?)(b|M|RC)\d.*/
 
