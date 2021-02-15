fb4c76800a35418a0ea7b205e2888b6346ff605d
Álvaro Sánchez-Mariscal <alvarosanchez@users.noreply.github.com>
2021-02-15 17:39:10 +0100

    Replace Bintray in favour of Maven Central, and use the new Sonatype infra (#111)

--- src/main/groovy/io/micronaut/build/MicronautPublishingPlugin.groovy
+++ src/main/groovy/io/micronaut/build/MicronautPublishingPlugin.groovy
@@ -1,5 +1,6 @@
 package io.micronaut.build
 
+import io.github.gradlenexus.publishplugin.NexusPublishExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Dependency
@@ -9,6 +10,7 @@ import org.gradle.api.plugins.ExtraPropertiesExtension
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.tasks.bundling.Jar
+import org.gradle.plugins.signing.Sign
 
 import java.time.Duration
 
@@ -18,7 +20,7 @@ import java.time.Duration
 class MicronautPublishingPlugin implements Plugin<Project> {
 
 
-    public static final String NEXUS_STAGING_PROFILE_ID = "4831469c7a1579"
+    public static final String NEXUS_STAGING_PROFILE_ID = "11bd7bc41716aa"
 
     @Override
     void apply(Project project) {
@@ -28,10 +30,7 @@ class MicronautPublishingPlugin implements Plugin<Project> {
 
         project.with {
             apply plugin: 'maven-publish'
-            apply plugin: 'com.jfrog.bintray'
             ExtraPropertiesExtension ext = extensions.getByType(ExtraPropertiesExtension)
-            def bintrayUser = System.getenv("BINTRAY_USER") ?: project.hasProperty("bintrayUser") ? project.bintrayUser : ''
-            def bintrayKey = System.getenv("BINTRAY_KEY") ?: project.hasProperty("bintrayKey") ? project.bintrayKey : ''
             def ossUser = System.getenv("SONATYPE_USERNAME") ?: project.hasProperty("sonatypeOssUsername") ? project.sonatypeOssUsername : ''
             def ossPass = System.getenv("SONATYPE_PASSWORD") ?: project.hasProperty("sonatypeOssPassword") ? project.sonatypeOssPassword : ''
             ext."signing.keyId" = System.getenv("GPG_KEY_ID") ?: project.hasProperty("signing.keyId") ? project.getProperty('signing.keyId') : null
@@ -94,17 +93,6 @@ class MicronautPublishingPlugin implements Plugin<Project> {
                 }
 
                 publishing {
-
-                    repositories {
-                        maven {
-                            credentials {
-                                username = bintrayUser
-                                password = bintrayKey
-                            }
-                            url  "https://oss.jfrog.org/oss-snapshot-local"
-                        }
-                    }
-
                     publications {
                         if (project.extensions.findByType(PublishingExtension).publications.empty) {
                             maven(MavenPublication) { publication ->
@@ -220,7 +208,6 @@ class MicronautPublishingPlugin implements Plugin<Project> {
             }
 
             if (ossUser && ossPass && ext."signing.keyId" && ext."signing.password") {
-                apply plugin: "de.marcphilipp.nexus-publish"
                 apply plugin: 'signing'
 
                 afterEvaluate {
@@ -231,64 +218,29 @@ class MicronautPublishingPlugin implements Plugin<Project> {
                     }
                 }
 
-                rootProject.plugins.apply('io.codearte.nexus-staging')
-                def extension = rootProject.extensions.getByName("nexusStaging")
-                extension.with {
-                    username = ossUser
-                    password = ossPass
-                    packageGroup = "io.micronaut"
-                    numberOfRetries = 500
-                    stagingProfileId = NEXUS_STAGING_PROFILE_ID
-                }
-
-                nexusPublishing {
-                    repositories {
-                        sonatype {
-                            username = ossUser
-                            password = ossPass
-                        }
-                    }
-                    connectTimeout = Duration.ofMinutes(30)
-                    clientTimeout = Duration.ofMinutes(30)
-                }
-
-                initializeSonatypeStagingRepository {
-                    timeout = Duration.ofMinutes(30)
-                }
-            }
-
-            bintray {
-                user = bintrayUser
-                key = bintrayKey
-                publications = ['maven']
-                publish = System.getenv('BINTRAY_PUBLISH') ? System.getenv('BINTRAY_PUBLISH').toBoolean() : true
-                pkg {
-                    repo = 'core-releases-local'
-                    userOrg = 'micronaut'
-                    name = project.name
-                    desc = "${project.title} - ${project.projectDesc} - ${project.name} module".toString()
-                    websiteUrl = projectUrl
-                    issueTrackerUrl = "https://github.com/$githubSlug/issues"
-                    vcsUrl = "https://github.com/$githubSlug"
-                    licenses = ['Apache-2.0']
-                    publicDownloadNumbers = true
-                    version {
-                        name = project.version
-                        gpg {
-                            sign = true
-                            passphrase = System.getenv("SIGNING_PASSPHRASE") ?: project.hasProperty("signingPassphrase") ? project.signingPassphrase : ''
+                rootProject.plugins.apply('io.github.gradle-nexus.publish-plugin')
+                NexusPublishExtension nexusPublish = rootProject.extensions.getByType(NexusPublishExtension)
+                nexusPublish.with {
+                    if (repositories.isEmpty()) {
+                        repositoryDescription = "${project.group}:${rootProject.name}:${project.version}"
+                        useStaging = !project.version.endsWith("-SNAPSHOT")
+                        repositories {
+                            sonatype {
+                                username = ossUser
+                                password = ossPass
+                                nexusUrl = uri("https://s01.oss.sonatype.org/service/local/")
+                                snapshotRepositoryUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+                                stagingProfileId = NEXUS_STAGING_PROFILE_ID //can reduce execution time by even 10 seconds
+                            }
                         }
                     }
                 }
-            }
 
-            afterEvaluate {
-                def publishTask = project.tasks.findByName('publishMavenPublicationToMavenRepository')
-                if (!project.version.endsWith("-SNAPSHOT") && publishTask) {
-                    // disable remote publish for non-snapshot versions
-                    // since releases are published to bintray
-                    publishTask.enabled = false
+                //do not generate extra load on Nexus with new staging repository if signing fails
+                tasks.withType(io.github.gradlenexus.publishplugin.InitializeNexusStagingRepository).configureEach {
+                    shouldRunAfter(tasks.withType(Sign))
                 }
+
             }
         }
     }
