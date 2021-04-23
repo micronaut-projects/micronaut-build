361bca2932f69203e418ddc5bdca7a34892cf11b
Utsav Varia <utsav99varia@gmail.com>
2021-04-23 15:25:59 +0530

    Fix trailing new line characters in github branch name (#137)
    * Fix trailing new line characters in github branch name
    
    * Move 'trim' to same execution line
    
    Fixes https://github.com/micronaut-projects/micronaut-docs/issues/23
    
    Co-authored-by: Iván López <lopezi@objectcomputing.com>

--- src/main/groovy/io/micronaut/build/MicronautDocsPlugin.groovy
+++ src/main/groovy/io/micronaut/build/MicronautDocsPlugin.groovy
@@ -200,7 +200,7 @@ class MicronautDocsPlugin implements Plugin<Project> {
                 dependsOn copyLocalDocResources
 
                 targetDir = file("${buildDir}/docs")
-                String githubBranch='git rev-parse --abbrev-ref HEAD'.execute()?.text ?: 'master'
+                String githubBranch='git rev-parse --abbrev-ref HEAD'.execute()?.text?.trim() ?: 'master'
                 sourceRepo = "https://github.com/${githubSlug}/edit/${githubBranch}/src/main/docs"
                 sourceDir = new File(projectDir, "src/main/docs")
                 propertiesFiles = [ new File(rootProject.projectDir, "gradle.properties") ]
