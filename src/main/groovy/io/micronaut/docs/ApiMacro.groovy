aadd218c03df4cf85deee68228f5a2af8b0df634
jameskleeh <james.kleeh@gmail.com>
2021-08-19 15:22:53 -0400

    Throw a more useful error message for invalid class names

--- src/main/groovy/io/micronaut/docs/ApiMacro.groovy
+++ src/main/groovy/io/micronaut/docs/ApiMacro.groovy
@@ -1,17 +1,17 @@
 /*
  * Copyright 2017 original authors
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 package io.micronaut.docs
 
@@ -122,6 +122,9 @@ class ApiMacro extends InlineMacroProcessor {
         for (int i = 0; i < arr.length; i++) {
             String token = arr[i]
             result += token
+            if (token.length() == 0) {
+                throw new StringIndexOutOfBoundsException("Invalid class string: " + str)
+            }
             if (Character.isUpperCase(token.charAt(0))) {
                 if (i != arr.length -1) {
                     result += "."
