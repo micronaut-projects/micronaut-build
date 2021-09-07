c13a67988136fdba1c8b0558ca5092f8cdc85d6a
Cedric Champeau <cedric.champeau@gmail.com>
2021-09-07 10:48:55 +0200

    Fix missing dependencyUpdates task
    Some CI jobs are calling `dependencyUpdates` which should be a
    no-op with the new version catalog update mechanism.

--- src/main/java/io/micronaut/build/catalogs/MicronautVersionCatalogUpdatePlugin.java
+++ src/main/java/io/micronaut/build/catalogs/MicronautVersionCatalogUpdatePlugin.java
@@ -7,6 +7,7 @@ import io.micronaut.build.catalogs.tasks.VersionCatalogUpdate;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.Copy;
+import org.gradle.api.tasks.TaskContainer;
 import org.gradle.api.tasks.TaskProvider;
 
 import java.util.Arrays;
@@ -17,17 +18,19 @@ public class MicronautVersionCatalogUpdatePlugin implements Plugin<Project> {
         if (project != project.getRootProject()) {
             throw new IllegalStateException("The " + MicronautVersionCatalogUpdatePlugin.class.getName() + " plugin must be applied on the root project only");
         }
-        TaskProvider<VersionCatalogUpdate> updater = project.getTasks().register("updateVersionCatalogs", VersionCatalogUpdate.class, task -> {
+        TaskContainer tasks = project.getTasks();
+        TaskProvider<VersionCatalogUpdate> updater = tasks.register("updateVersionCatalogs", VersionCatalogUpdate.class, task -> {
             task.getCatalogsDirectory().convention(project.getLayout().getProjectDirectory().dir("gradle"));
             task.getOutputDirectory().convention(project.getLayout().getProjectDirectory().dir("gradle/updates"));
             task.getRejectedQualifiers().convention(Arrays.asList("alpha", "beta", "rc", "cr", "m", "preview", "b", "ea"));
             task.getIgnoredModules().convention(Collections.emptySet());
             task.getAllowMajorUpdates().convention(false);
         });
-        project.getTasks().register("useLatestVersions", Copy.class, task -> {
+        tasks.register("useLatestVersions", Copy.class, task -> {
             VersionCatalogUpdate dependent = updater.get();
             task.from(dependent.getOutputDirectory());
             task.into(dependent.getCatalogsDirectory());
         });
+        tasks.register("dependencyUpdates", task -> task.setDescription("Compatibility task with the old update mechanism"));
     }
 }
