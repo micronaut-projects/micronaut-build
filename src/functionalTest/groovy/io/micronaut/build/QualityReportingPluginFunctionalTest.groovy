638a9bf27c3acff21c1855399d8ac50f70daac9e
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2022-02-17 13:15:56 +0100

    Fix Checkstyle plugin configuration (#278)

--- src/functionalTest/groovy/io/micronaut/build/QualityReportingPluginFunctionalTest.groovy
+++ src/functionalTest/groovy/io/micronaut/build/QualityReportingPluginFunctionalTest.groovy
@@ -18,4 +18,23 @@ class QualityReportingPluginFunctionalTest extends AbstractFunctionalTest {
         file("build/reports/jacoco/testCodeCoverageReport/testCodeCoverageReport.xml").exists()
     }
 
+    void "it can run Spotless and Checkstyle"() {
+        given:
+        withSample("test-micronaut-module")
+
+        when:
+        run 'spotlessApply', 'checkstyleMain'
+
+        then:
+        tasks {
+            succeeded ':subproject1:spotlessCheck'
+            succeeded ':subproject1:checkstyleMain'
+
+            succeeded ':subproject2:spotlessCheck'
+            succeeded ':subproject2:checkstyleMain'
+        }
+        file("subproject1/build/reports/checkstyle/main.xml").exists()
+        file("subproject2/build/reports/checkstyle/main.xml").exists()
+    }
+
 }
