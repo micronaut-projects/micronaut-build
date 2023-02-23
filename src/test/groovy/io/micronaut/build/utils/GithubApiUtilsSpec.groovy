b2864e5b6acf26c5a12b8600ffd7a5468a0537c7
Cedric Champeau <cedric.champeau@gmail.com>
2023-02-23 12:01:33 +0100

    Mock GitHub API
    This commit mocks the GitHub API in tests.

--- src/test/groovy/io/micronaut/build/utils/GithubApiUtilsSpec.groovy
+++ src/test/groovy/io/micronaut/build/utils/GithubApiUtilsSpec.groovy
@@ -2,14 +2,55 @@ package io.micronaut.build.utils
 
 import org.gradle.api.GradleException
 import org.gradle.api.logging.Logger
+import org.mockserver.integration.ClientAndServer
+import org.mockserver.model.MediaType
+import spock.lang.Shared
 import spock.lang.Specification
+import spock.util.environment.RestoreSystemProperties
+import static org.mockserver.model.HttpRequest.request
+import static org.mockserver.model.HttpResponse.response
 
+@RestoreSystemProperties
 class GithubApiUtilsSpec extends Specification {
+
+    @Shared
+    private ClientAndServer mockServer
+
+    def setupSpec() {
+        mockServer = ClientAndServer.startClientAndServer()
+        ['tags', 'releases'].each { what ->
+            mockServer.when(
+                    request()
+                            .withMethod("GET")
+                            .withPath("/repos/micronaut-projects/micronaut-security/$what")
+            ).respond(
+                    response()
+                            .withStatusCode(200)
+                            .withContentType(MediaType.JSON_UTF_8)
+                            .withBody(GithubApiUtilsSpec.getResourceAsStream("/io.micronaut.build.utils/releases.json").bytes)
+            )
+            mockServer.when(
+                    request()
+                            .withMethod("GET")
+                            .withPath("/repos/micronaut-projects/nope/$what")
+            ).respond(
+                    response()
+                            .withStatusCode(404)
+                            .withBody("Not found")
+            )
+        }
+
+        System.setProperty(GithubApiUtils.GITHUB_API_BASE_URL_SYSTEM_PROPERTY, "http://localhost:${mockServer.localPort}")
+    }
+
+    def cleanupSpec() {
+        mockServer.stop()
+    }
 
     void "it is possible to fetch tags"() {
         when:
         String tags = new String(GithubApiUtils.fetchTagsFromGitHub(Stub(Logger), "micronaut-projects/micronaut-security"), "UTF-8")
-
+        println tags
         then:
         noExceptionThrown()
         tags.contains("v")
@@ -18,7 +59,7 @@ class GithubApiUtilsSpec extends Specification {
     void "it is possible to fetch releases"() {
         when:
         String releases = new String(GithubApiUtils.fetchReleasesFromGitHub(Stub(Logger), "micronaut-projects/micronaut-security"), "UTF-8")
-
+        println releases
         then:
         noExceptionThrown()
         releases.contains("3.")
@@ -37,5 +78,4 @@ class GithubApiUtilsSpec extends Specification {
             assert message.startsWith("Failed to read from Github API. Response code: 404")
         }
     }
-
 }
