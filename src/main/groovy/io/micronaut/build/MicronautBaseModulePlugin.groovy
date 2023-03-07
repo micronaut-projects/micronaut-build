aa997e489e0b7ffd4dbbe68ec7c983ff40e943d2
Cedric Champeau <cedric.champeau@gmail.com>
2023-03-07 15:15:29 +0100

    Introduce pre-release check task
    The `preReleaseCheck` task is designed to be executed when a module
    is still in SNAPSHOT state, but we want to make sure that it is ready
    for release.
    
    For now, the task will only trigger the verification of the generated
    POM file (and their transitive dependencies), in order to check that
    a release doesn't contain a dependency on a SNAPSHOT.
    
    It is possible to bypass the failures by running with the
    `micronaut.fail.on.snapshots` system property set to `true`.

--- src/main/groovy/io/micronaut/build/MicronautBaseModulePlugin.groovy
+++ src/main/groovy/io/micronaut/build/MicronautBaseModulePlugin.groovy
@@ -2,11 +2,12 @@ package io.micronaut.build
 
 import groovy.transform.CompileStatic
 import io.micronaut.build.compat.MicronautBinaryCompatibilityPlugin
+import io.micronaut.build.pom.PomCheckerUtils
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.services.BuildServiceSpec
 import org.gradle.api.tasks.testing.Test
-
 /**
  * Configures a project as a typical Micronaut module project:
  *  - with dependency updates plugin
@@ -24,6 +25,9 @@ class MicronautBaseModulePlugin implements Plugin<Project> {
         project.pluginManager.apply(MicronautBinaryCompatibilityPlugin)
         configureJUnit(project)
         assertSettingsPluginApplied(project)
+        PomCheckerUtils.registerPomChecker("checkPom", project, project.extensions.findByType(PublishingExtension)) {
+            it.suppressions.convention(project.extensions.getByType(MicronautBuildExtension).bomSuppressions)
+        }
     }
 
     private static void configureJUnit(Project project) {
