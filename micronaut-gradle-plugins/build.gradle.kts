29a6db13c6f814fd65c8a6168146214fe3b221b1
Álvaro Sánchez-Mariscal <153880+alvarosanchez@users.noreply.github.com>
2026-06-17 11:26:53 +0000

    test: add Gradle API to test runtime

--- micronaut-gradle-plugins/build.gradle.kts
+++ micronaut-gradle-plugins/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     }
 
     testImplementation(libs.typesafe.config)
+    testRuntimeOnly(gradleApi())
 
     testImplementation(libs.mockserver.server)
     testImplementation(libs.mockserver.client)
